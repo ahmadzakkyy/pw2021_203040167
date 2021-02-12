1defe7ad6630b3ee2719918fe6eac62ea3eb1547
ahmadzakkyy <ahmadzakkyy12@gmail.com>
2021-02-12 22:55:44 +0700

    menyelesaikan video pertemuan 2

---
+++ pertemuan 2/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Ahmad Hannan Muzakky
+203040167
+https://github.com/ahmadzakkyy/pw2021_203040167
+pertemuan 2 - 12 Februari 2021
+Mempelajari Mengenai Syntax php
+*/
+?>
+<?php
+// Standar Output
+// echo, print
+?>
