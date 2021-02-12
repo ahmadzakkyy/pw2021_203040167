84707a801cc9da7df4bd5402d774f3d0225e22ec
ahmadzakkyy <ahmadzakkyy12@gmail.com>
2021-02-12 23:33:20 +0700

    menyelesaikan video pertemuan 2

--- pertemuan 2/index.php
+++ pertemuan 2/index.php
@@ -1,13 +1,36 @@
-<?php
+<?php 
 /*
 Ahmad Hannan Muzakky
 203040167
 https://github.com/ahmadzakkyy/pw2021_203040167
-pertemuan 2 - 12 Februari 2021
-Mempelajari Mengenai Syntax php
+Pertemuan 2 - 12 Februari 2021
+Mempelajari mengenai syntax PHP
 */
 ?>
+
+
 <?php
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
 // Standar Output
 // echo, print
-?>
+// print_r
+// var_dump
+
+// Penulisan sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh di awali dengan angka, tapi boleh mengandung angka
+// $nama = "Ahmad Zakky";
+// echo "Halo, nama saya $nama";
+
+// Operator
+// Aritmatika
+// + - * / %
+// $x = 10;
+// $y = 20;
+// echo $x * $y;
